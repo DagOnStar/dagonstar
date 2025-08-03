5c9cf2d958ca2956a8931871f435dc247563280b
Mona Lisa <dante310395@gmail.com>
2025-08-03 10:45:03 +0000

    resilience results

---
+++ tests/benchmarks/dynostore/run_failures_tests.sh
@@ -0,0 +1,4 @@
+python3 failures.py --server 192.5.86.169:80 --object 5fb011c2-f01a-4f16-86fe-d3d45a2c95d0 --repeats 100 --csv failures_10_3_90percent.csv
+python3 failures.py --server 192.5.86.169:80 --object 68cd5599-c0f9-44d1-9ad4-424cd0a47bdf --repeats 100 --csv failures_10_5_90percent.csv
+python3 failures.py --server 192.5.86.169:80 --object 4a604d9a-9069-4c2f-a4d5-f2529bb3673c --repeats 100 --csv failures_10_7_90percent.csv
+python3 failures.py --server 192.5.86.169:80 --object 619a8da1-f90c-41e5-9e1e-0c2d2b7b8489 --repeats 100 --csv failures_10_9_90percent.csv
